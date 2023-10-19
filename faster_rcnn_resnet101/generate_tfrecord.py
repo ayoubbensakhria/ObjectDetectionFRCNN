80393edb8ba5e8e48f8081d32c21408b2d55ec3b
Ayoub Bensakhria <54290049+ayoubbensakhria@users.noreply.github.com>
2023-10-19 14:16:46 +0100

    Create generate_tfrecord.py

---
+++ faster_rcnn_resnet101/generate_tfrecord.py
@@ -0,0 +1,62 @@
+import os
+import io
+import pandas as pd
+import tensorflow as tf
+from PIL import Image
+
+from object_detection.utils import dataset_util
+from collections import namedtuple
+
+def class_text_to_int(row_label):
+    if row_label == 'Black Kiterotation':
+        return 1
+    # Add more labels if necessary
+    else:
+        None
+
+def create_tf_example(group, path):
+    with tf.io.gfile.GFile(os.path.join(path, '{}'.format(group.filename)), 'rb') as fid:
+        encoded_jpg = fid.read()
+    encoded_jpg_io = io.BytesIO(encoded_jpg)
+    image = Image.open(encoded_jpg_io)
+    width, height = image.size
+
+    filename = group.filename.encode('utf8')
+    image_format = b'jpg'
+    xmins = [group.xmin / width]
+    xmaxs = [group.xmax / width]
+    ymins = [group.ymin / height]
+    ymaxs = [group.ymax / height]
+    classes_text = group['class'].encode('utf8')
+    classes = class_text_to_int(group['class'])
+
+    tf_example = tf.train.Example(features=tf.train.Features(feature={
+        'image/height': dataset_util.int64_feature(height),
+        'image/width': dataset_util.int64_feature(width),
+        'image/filename': dataset_util.bytes_feature(filename),
+        'image/source_id': dataset_util.bytes_feature(filename),
+        'image/encoded': dataset_util.bytes_feature(encoded_jpg),
+        'image/format': dataset_util.bytes_feature(image_format),
+        'image/object/bbox/xmin': dataset_util.float_list_feature(xmins),
+        'image/object/bbox/xmax': dataset_util.float_list_feature(xmaxs),
+        'image/object/bbox/ymin': dataset_util.float_list_feature(ymins),
+        'image/object/bbox/ymax': dataset_util.float_list_feature(ymaxs),
+        'image/object/class/text': dataset_util.bytes_feature(classes_text),
+        'image/object/class/label': dataset_util.int64_feature(classes),
+    }))
+    return tf_example
+def main(csv_file, output_tfrecord, image_folder):
+    examples = pd.read_csv(csv_file)
+    writer = tf.io.TFRecordWriter(output_tfrecord)
+    for index, group in examples.groupby('filename'):
+        tf_example = create_tf_example(group, image_folder)
+        writer.write(tf_example.SerializeToString())
+    writer.close()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--csv_file', required=True, help='Input CSV file path')
+    parser.add_argument('--output_tfrecord', required=True, help='Output TFRecord file path')
+    parser.add_argument('--image_folder', required=True, help='Path to image folder')
+    args = parser.parse_args()
+    main(args.csv_file, args.output_tfrecord, args.image_folder)
