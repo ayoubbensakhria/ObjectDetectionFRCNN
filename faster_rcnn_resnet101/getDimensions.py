7fd06f82674da335b5d289f2d3d1aad25a55d9c5
Ayoub Bensakhria <54290049+ayoubbensakhria@users.noreply.github.com>
2023-10-19 09:23:54 +0100

    Update getDimensions.py

--- faster_rcnn_resnet101/getDimensions.py
+++ faster_rcnn_resnet101/getDimensions.py
@@ -1,69 +1,36 @@
 import os
-import glob #<- search files and folders in a path
+import glob
 import argparse
 import xml.etree.ElementTree as ET
 from matplotlib.pyplot import imread
 
-
 def get_dimensions(path, extension="jpg"):
-    """Iterates through all .jpg files in all subdirectories.
-
-    Parameters:
-    ----------
-    path : {str}
-        The path containing the .jpg files
-    Returns
-    -------
-    two lists of heights and widths
-    """
-    # list of tuples containing heights and widths
     hw_pairs = []
-    # list of classes with annotation count
     annotation_count = {}
-    # Search recursively in subdirectories
-    for jpg_file in glob.glob(path + '/**/*.{ext}'.format(ext=extension), recursive=True):
+    for xml_file in glob.glob(path + '/**/*.xml', recursive=True):
         try:
-            # get current folder
-            current_folder = os.path.basename(os.path.dirname(jpg_file))
-            # Exclude test, valid
-            if current_folder in ['test', 'valid']:
-                continue
-            img = imread(jpg_file)
-            # check if xml with the same filename exists
-            xml_filename = os.path.splitext(jpg_file)[0] + '.xml'
-            annotated = 1 if os.path.exists(xml_filename) else 0
-            # get height and width
-            height, width, _ = img.shape
-            # update annotation count
-            if current_folder in annotation_count:
-                annotation_count[current_folder] += annotated
+            tree = ET.parse(xml_file)
+            root = tree.getroot()
+            width = int(root.find('.//size/width').text)
+            height = int(root.find('.//size/height').text)
+            hw_pairs.append((height, width))
+            
+            current_class = root.find('.//object/name').text
+            if current_class in annotation_count:
+                annotation_count[current_class] += 1
             else:
-                annotation_count[current_folder] = annotated
-            # check class
-            if height is not None and width is not None:
-                hw_pairs.append((height,width))
-            else:
-                continue
-        except:
-            print('An arror occured with the file:', jpeg_file)
+                annotation_count[current_class] = 1
+        except Exception as e:
+            print(f'An error occurred with the file: {xml_file}. Error: {e}')
             continue
-    print(hw_pairs) # <- index 0
-    print (annotation_count) # <- index 1
 
+    print(hw_pairs)
+    print(annotation_count)
 
 def main():
-    # Initiate argument parser
-    parser = argparse.ArgumentParser(
-        description="Get Image Dimensions")
-    parser.add_argument("-i",
-                        "--inputDir",
-                        help="Root path to the folder where the input image files are stored",
-                        type=str)
-    parser.add_argument("-ex",
-                        "--extension",
-                        help="Image extension",
-                        default="jpg",
-                        type=str)
+    parser = argparse.ArgumentParser(description="Get Image Dimensions")
+    parser.add_argument("-i", "--inputDir", help="Root path to the folder where the input image files are stored", type=str)
+    parser.add_argument("-ex", "--extension", help="Image extension", default="jpg", type=str)
     args = parser.parse_args()
 
     if(args.inputDir is None):
