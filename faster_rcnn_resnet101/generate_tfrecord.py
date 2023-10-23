7c4356e5e0352534fe316dd4ca88c8925c0129b2
Ayoub Bensakhria <54290049+ayoubbensakhria@users.noreply.github.com>
2023-10-23 10:57:43 +0100

    Update generate_tfrecord.py

--- faster_rcnn_resnet101/generate_tfrecord.py
+++ faster_rcnn_resnet101/generate_tfrecord.py
@@ -4,7 +4,7 @@ Usage:
   # Create train data:
   python generate_tfrecord.py --csv_input=images/train_labels.csv --image_dir=images/train --output_path=train.record
 
-  # Create test data:
+  # Create test data: 
   python generate_tfrecord.py --csv_input=images/test_labels.csv  --image_dir=images/test --output_path=test.record
 """
 from __future__ import division
